d75ef8831f3476ecc723c39653f2a44f6cfa6b86
LenaAshqar <s202282660@kfupm.edu.sa>
2025-10-25 16:14:24 +0300

    changed projects section

--- assignment-2/src/components/projects/ProjectCard.jsx
+++ assignment-2/src/components/projects/ProjectCard.jsx
@@ -1,15 +1,36 @@
-import React from "react";
-import "./Projects.css";
-import "../FrostedGlass.css";
+import { FiArrowUpRight } from 'react-icons/fi';
+import './Projects.css';
 
-const ProjectCard = ({ img, title, description }) => {
-    return (
-        <article className="project-card frostedGlass">
-            <img src={img} alt={title} />
-            <h4>{title}</h4>
+const ProjectCard = ({ image, title, description, tags = [], year, link }) => (
+    <article className="project-card surface-card">
+        <div className="project-media">
+            <img src={image} alt={title} />
+            {year && <span className="project-year">{year}</span>}
+        </div>
+        <div className="project-body">
+            <h3>{title}</h3>
             <p>{description}</p>
-        </article>
-    );
-};
-
+            {tags.length > 0 && (
+                <ul className="project-tags" role="list">
+                    {tags.map(tag => (
+                        <li key={tag}>{tag}</li>
+                    ))}
+                </ul>
+            )}
+            <div className="project-footer">
+                {link ? (
+                    <a className="project-link" href={link} target="_blank" rel="noreferrer noopener">
+                        View project
+                        <FiArrowUpRight aria-hidden="true" />
+                    </a>
+                ) : (
+                    <span className="project-link project-link--muted">
+                        Case study coming soon
+                        <FiArrowUpRight aria-hidden="true" />
+                    </span>
+                )}
+            </div>
+        </div>
+    </article>
+);
 export default ProjectCard;
